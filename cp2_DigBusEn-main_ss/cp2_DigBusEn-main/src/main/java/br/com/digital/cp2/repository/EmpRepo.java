797394c5350361a6c103b6400878a116c3627211
Carlos Silva <111822291+CarlosQw32@users.noreply.github.com>
2023-11-06 23:23:07 -0300

    Delete cp2_DigBusEn-main_ss/cp2_DigBusEn-main directory

--- cp2_DigBusEn-main_ss/cp2_DigBusEn-main/src/main/java/br/com/digital/cp2/repository/EmpRepo.java
+++
@@ -1,7 +0,0 @@
-package br.com.digital.cp2.repository;
-
-import br.com.digital.cp2.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpRepo extends JpaRepository<Employee, Long> {
-}
